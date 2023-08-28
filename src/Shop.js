1be513959148f65c09a7d673a88a6b3fd3dec4cf
Mikhail Agapov <mr.hapon@gmail.com>
2023-08-28 22:41:00 +0300

    add shop sorting

--- src/Shop.js
+++ src/Shop.js
@@ -1,13 +1,54 @@
-import { NavLink } from 'react-router-dom'
+import { NavLink, useLocation } from 'react-router-dom'
+import queryString from 'query-string'
+import { useState } from 'react'
 import books from './data/books'
 import './shop.css'
 
+function sortBooks(books, key) {
+  const sortedBooks = [...books]
+  if (!key) {
+    return sortedBooks
+  }
+  sortedBooks.sort((a, b) => (a[key] > b[key] ? 1 : -1))
+  return sortedBooks
+}
+
 const Shop = () => {
+  const location = useLocation()
+  const query = queryString.parse(location.search)
+  const [sortKey, setSortKey] = useState(query.sort)
+  const [sortedBooks, setSortedBooks] = useState(sortBooks(books, sortKey))
+
+  function newSort() {
+    setSortKey(query.sort)
+    setSortedBooks(sortBooks(books, sortKey))
+    console.log(sortKey)
+  }
+  // useEffect(() => newSort(), [])
+
   return (
     <>
       <h2 className="shopTitle">Магазин</h2>
+      <div className="sort">
+        <button className="sortButton">Сортировать:</button>
+        <div className="sortList">
+          <NavLink
+            className="sortLink"
+            to=".?sort=author"
+            onClick={() => newSort()}
+          >
+            По автору
+          </NavLink>
+          <NavLink className="sortLink" to=".?sort=title">
+            По названию
+          </NavLink>
+          <NavLink className="sortLink" to=".?sort=price">
+            По цене
+          </NavLink>
+        </div>
+      </div>
       <div className="shopPage">
-        {books.map((book) => (
+        {sortedBooks.map((book) => (
           <div className="singleBook">
             <NavLink to={book.title} key={book.id}>
               <img
@@ -18,6 +59,7 @@ const Shop = () => {
               />
               <h2>{book.title}</h2>
               <h3>{book.author}</h3>
+              <h3>{book.price + ' руб'}</h3>
             </NavLink>
           </div>
         ))}
